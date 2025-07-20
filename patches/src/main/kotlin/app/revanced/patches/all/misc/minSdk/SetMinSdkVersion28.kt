8e747870ad90741ee0274c19c492c020836aa1b0
Vadász András <csagataj2@gmail.com>
2025-07-20 18:51:20 +0200

    Fix comma

--- patches/src/main/kotlin/app/revanced/patches/all/misc/minSdk/SetMinSdkVersion28.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/minSdk/SetMinSdkVersion28.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.all.misc.targetSdk
+package app.revanced.patches.all.misc.minSdk
 
 import app.revanced.patcher.patch.resourcePatch
 import app.revanced.util.getNode
@@ -6,9 +6,9 @@ import org.w3c.dom.Element
 import java.util.logging.Logger
 
 @Suppress("unused")
-val setTargetSdkVersion28 = resourcePatch(
+val setMinSdkVersion28 = resourcePatch(
     name = "Set target SDK version 28",
-    description = "Changes the target SDK to version 28 (Android 9). "
+    description = "Changes the target SDK to version 28 (Android 9). ",
     use = false,
 ) {
     execute {
@@ -17,25 +17,7 @@ val setTargetSdkVersion28 = resourcePatch(
         document("AndroidManifest.xml").use { document ->
             fun getLogger() = Logger.getLogger(this::class.java.name)
 
-            // Ideally, the override should only be applied if the existing target is higher.
-            // But since ApkTool does not add targetSdkVersion to the decompiled AndroidManifest,
-            // there is no way to check targetSdkVersion. Instead, check compileSdkVersion and print a warning.
-            try {
-                val manifestElement = document.getNode("manifest") as Element
-                val compileSdkVersion = Integer.parseInt(
-                    manifestElement.getAttribute("android:compileSdkVersion")
-                )
-                if (compileSdkVersion <= minSdkOverride) {
-                    getLogger().warning(
-                        "This app does not appear to use a target SDK above $minSdkOverride: " +
-                                "(compileSdkVersion: $compileSdkVersion)"
-                    )
-                }
-            } catch (_: Exception) {
-                getLogger().warning("Could not check compileSdkVersion")
-            }
-
-            // Change targetSdkVersion to override value.
+            // Change minSdkVersion to override value.
             document.getElementsByTagName("manifest").item(0).let {
                 var element = it.ownerDocument.createElement("uses-sdk")
                 element.setAttribute("android:minSdkVersion", minSdkOverride.toString())
